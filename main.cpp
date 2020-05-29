38e18e33eb1693d9c410bc3910013e7953508c54
empty-works <pama.mark@gmail.com>
2020-05-29 04:43:08 +0200

    Testing vector iteration without the STL.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iterator>
 
 int main() {
 
 	std::vector<std::string> str_vec {};
 
-	// No STL 
+	// No STL
+	str_vec.push_back("Hey");
+	str_vec.push_back("there, ");
+	str_vec.push_back("partner!");
+	
+	std::cout << "Without STL: " << std::endl;
+	for(auto &str : str_vec) {
+		std::cout << str << std::endl;
+	}	
+
 	return 0;
 }
